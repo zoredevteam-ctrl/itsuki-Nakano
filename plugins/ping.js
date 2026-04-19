4e22033ebc8ad29941276c5c1d0adfb2a9040892
zoredevteam-ctrl <zoredevteam@gmail.com>
2026-04-19 11:39:50 -0500

    Actualizar ping.js

--- plugins/ping.js
+++ plugins/ping.js
@@ -1,1 +1,20 @@
-u
+export default {
+    command: ['ping', 'p'],
+    tags: ['info'],
+    help: ['ping', 'p'],
+    desc: 'Muestra la latencia del bot',
+
+    run: async (m, { conn, usedPrefix, command, isOwner }) => {
+        const start = Date.now()
+        const latency = Date.now() - start
+
+        const pingText = `乂 *PING - ITSUKI NAKANO* 乂
+
+*Latencia:* ${latency} ms
+*Ping WS:* ${conn?.ws?.ping ?? 'N/A'} ms
+
+¡Todo listo para ti, ${isOwner ? 'dueño ❤️' : 'amigo'}! 🍀`
+
+        await m.reply(pingText)
+    }
+}
